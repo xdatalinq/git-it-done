9956cf7b253992cf70bbf10b8496026479acb1c0
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-03-29 11:26:48 -0400

    added message if 30+ issues in repo

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
-
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -51,7 +51,29 @@ var displayIssues = function(issues) {
 };
 
 var getRepoIssues = function(repo) {
-    displayIssues(data)
+    if (response.ok) {
+        response.json().then(function(data) {
+          displayIssues(data);
+      
+          // check if api has paginated issues
+          if (response.headers.get("Link")) {
+            displayWarning(repo);
+          }
+        });
+      }
   };
 
 getRepoIssues("facebook/react");
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
+
